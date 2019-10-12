2c2ee61bf2b7c34775a6a0c748971ffe9cd3bfa5
juliankominiak-ps <56292505+juliankominiak-ps@users.noreply.github.com>
2019-10-12 21:22:17 +0200

    Update losowa_liczba(tkinter).py

--- losowa_liczba(tkinter).py
+++ losowa_liczba(tkinter).py
@@ -28,64 +28,64 @@ def check():
     if box.get().isdigit():
         if int(box.get()) < 0 or int(box.get()) > 100:
             box_input = -1
-            feedback = Label(Okno, text="Twoja liczba musi zawierać się \n w przedziale od 0 do 100.")
+            feedback = Label(window, text="Twoja liczba musi zawierać się \n w przedziale od 0 do 100.")
         else:
             box_input = int(box.get())
-            feedback = Label(Okno, text="Twoja liczba: " + str(box_input))
+            feedback = Label(window, text="Twoja liczba: " + str(box_input))
     else:
         box_input = -1
-        feedback = Label(Okno, text="Możesz wpisywać tylko liczby")
+        feedback = Label(window, text="Możesz wpisywać tylko liczby")
 
     box.delete(0, 'end')
     feedback.pack()
 
     if box_input < random_number:
-        less_label = Label(Okno, text="za mała liczba", fg="red")
+        less_label = Label(window, text="za mała liczba", fg="red")
         less_label.pack()
         try_count += 1
 
     if box_input > random_number:
-        more_label = Label(Okno, text="za duża liczba", fg="red")
+        more_label = Label(window, text="za duża liczba", fg="red")
         more_label.pack()
         try_count += 1
 
     if box_input == random_number:
-        win_label = Label(Okno, text="brawo, mój przyjacielu", fg="orange")
+        win_label = Label(window, text="brawo, mój przyjacielu", fg="orange")
         win_label.pack()
         success = True
         try_count += 1
 
-    try_label = Label(Okno, text="Ilość prób: " + str(try_count), fg="green")
+    try_label = Label(window, text="Ilość prób: " + str(try_count), fg="green")
     try_label.pack()
 
     check_button.destroy()
 
     if success:
-        quit_button = tkinter.Button(Okno, text="Zakończ grę", bg="red", command=esc)
+        quit_button = tkinter.Button(window, text="Zakończ grę", bg="red", command=esc)
         quit_button.pack()
 
-    retry_button = tkinter.Button(Okno, text="Spróbuj jeszcze raz!", command=reset)
+    retry_button = tkinter.Button(window, text="Spróbuj jeszcze raz!", command=reset)
     retry_button.pack()
 
 
 def esc():
-    Okno.destroy()
+    window.destroy()
 
 
 success = 0
 try_count = 0
 random_number = random.randint(1, 100)
 
-Okno = Tk()
-Okno.title("Moduł I, temat 8")
+window = Tk()
+window.title("Moduł I, temat 8")
 
-task = Label(Okno, text="Wpisz liczbe")
+task = Label(window, text="Wpisz liczbe")
 task.pack()
 
-box = Entry(Okno, bd=5)
+box = Entry(window, bd=5)
 box.pack()
 
-check_button = tkinter.Button(Okno, text="Sprawdź czy trafiłeś!", command=check)
+check_button = tkinter.Button(window, text="Sprawdź czy trafiłeś!", command=check)
 check_button.pack()
 
-Okno.mainloop()
+window.mainloop()
